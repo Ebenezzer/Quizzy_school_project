af53d8684b1881f40beccd9dd7233424d1b37570
anmp <anmp@kth.se>
2022-12-02 13:18:30 +0100

    working on css log in page

--- src/Views/loginView/loginView.js
+++ src/Views/loginView/loginView.js
@@ -1,4 +1,4 @@
-
+import './loginView.css';
 
 function LoginView(props){
 
@@ -18,9 +18,12 @@ function LoginView(props){
         props.sendPassword(password.target.value)
     }
 
-    return <div>
-        <input type = {"email"} placeholder = "enter your email" onChange = {setEmailACB}></input>
-        <input type = {"password"} placeholder = "enter your password" onChange = {setPasswordACB}></input>
+    return <div className = "container">
+        <label for = "email"><b>Email</b></label>
+        <input type = {"email"} placeholder = "enter your email" onChange = {setEmailACB} name="email" required></input>
+
+        <label for = "password"><b>Password</b></label>
+        <input type = {"password"} placeholder = "enter your password" onChange = {setPasswordACB} name="password" required></input>
 
         <button onClick = {clickLogInACB}>Log In</button>
         <button onClick={clickCreateAccountACB}>Create Account</button>
