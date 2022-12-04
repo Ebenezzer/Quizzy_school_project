2dbe4a05a015c190e1e4f8f57aa9e7b2c44ab928
Edina Andreea Niculescu <eani@kth.se>
2022-12-04 16:09:27 +0100

    New game pup up
    Pup up fixed

--- src/Views/homeView/homeView.js
+++ src/Views/homeView/homeView.js
@@ -1,7 +1,32 @@
+import "./homeView.css"
+
 function HomeView(props){
+    function buttonOnClick(){
+        var model = document.getElementById('model');
+        model.style.display = 'block';
+    }
+    function closeModel(){
+        var model = document.getElementById('model');
+        model.style.display = 'none';
+    }
+
+    function onInviteClick(){
+        var username = document.getElementById('username').value;
+        console.log("Invite sent to: " + username);
+    }
+    
     return <div>
          <h1>Quizzy</h1>
-         <button className="">new game</button>
+         <button onClick={buttonOnClick} className="new-game">New game</button>
+         <div className="popup-model" id="model">
+        <div className="modal-content">
+         <span className="close" onClick={closeModel}>&times;</span>
+            <h2>New Game</h2>
+            <label>Username</label> <br></br>
+            <input type="text" id="username"></input>
+            <button className="btn-invite" onClick={onInviteClick}>Invite</button>
+         </div>
+         </div>
          </div>
 }
 
--- src/components/gameList/gameList.js
+++ src/components/gameList/gameList.js
@@ -3,9 +3,11 @@ import "./gameList.css"
 export default
 function GameList(props){ 
     function renderGameDetails(games){
-        const onClick = () => console.log("game between" + games.player1 + " and " + games.player2);
+        function doOnClick(){
+            console.log("game between" + games.player1 + " and " + games.player2)   
+        }
             return (
-                <div className="game-list" onClick={onClick}> 
+                <div className="game-list" onClick={doOnClick}> 
                     <img className="player-icon" src="https://cdn4.iconfinder.com/data/icons/symbols-vol-1-1/40/user-person-single-id-account-player-male-female-512.png"></img>
                     <p>{games.player1}</p>
                     <p className="vs-span"> VS </p>
