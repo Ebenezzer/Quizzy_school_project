456aeac30b2ca54d0a8c778679877117878bcc7d
Edina Andreea Niculescu <eani@kth.se>
2022-12-05 14:08:18 +0100

    Fixed merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
-<<<<<<< HEAD
 import Show from './components/show/show';
 import Game from './Presenters/gamePresenter';
 import QuizModel from './QuizModel';
+import Home from './Presenters/homePresenter';
+
+export default
 
 function App() {
   const model = new QuizModel();
   return (
     <div className="App">
       <Game model={model}/>
-=======
-import Home from './Presenters/homePresenter';
-
-export default
-function App(props) {
-  return (
-    <div className="App">
       <Home/>
->>>>>>> edina's-branch
-    </div>
+      </div>
   );
 }
 
