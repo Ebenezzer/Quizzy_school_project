a935f9747a555bde16d275dc46d753adbdaabb73
Edina Andreea Niculescu <eani@kth.se>
2022-12-05 13:58:14 +0100

    Active games filtered

--- src/Presenters/homePresenter.js
+++ src/Presenters/homePresenter.js
@@ -6,40 +6,50 @@ export default
 function Home(props){
     function getMyGamesCB(object)
     { 
-        return object.currentRound !== "username1"; 
+        return object.currentRound === "username1"; 
     }
 
     function getOpponentsGamesCB(object)
     { 
-        return object.currentRound === "username1";
+        return object.currentRound !== "username1";
     }
+
+    function getActiveGames(object)
+    {
+     return !object.winner;
+    }
+
     const currentGames = [
         {
             player1: "username1",
             player2: "username2",
             currentRound: "username1",
+            winner: null
         },
         {
             player1: "username1",
             player2: "username2",
             currentRound: "username2",
+            winner: "username2"
         },
         {
             player1: "username1",
             player2: "username6",
             currentRound: "username6",
+            winner: null
         },
         {
             player1: "username5",
             player2: "username1",
             currentRound: "username1",
+            winner: "username1"
         }
 
     ]
+
  return <div>
     <HomeView/>
-    <GameList currentGame = {currentGames.filter(getMyGamesCB)} turn = {"Your turn"}/>
-    <GameList currentGame = {currentGames.filter(getOpponentsGamesCB)} turn = {"Opponent's turn"}/>
-
+    <GameList currentGame = {currentGames.filter(getActiveGames).filter(getMyGamesCB)} turn = {"Your turn"}/>
+    <GameList currentGame = {currentGames.filter(getActiveGames).filter(getOpponentsGamesCB)} turn = {"Opponent's turn"}/>
     </div>
 }
