2f9bf7190729f884f3fe7a67558ab62403cec016
David Persson <davpers@kth.se>
2022-12-13 14:22:56 +0100

    Clean up

--- src/Views/gameview/gameView.js
+++ src/Views/gameview/gameView.js
@@ -1,31 +1,41 @@
-import React, { useState } from "react";
-import Question from "../../components/Question/Question";
-import { shuffleArray } from "../../helpFunctions";
+import React from "react";
+import { FiArrowRight } from 'react-icons/fi';
 import "./gameView.css"
+import Answer from '../Answer'
 
 export default
 function GameView(props){
+    function updateCurrentAnswerACB(answer){
+        props.onUpdateCurrentAnswer(answer)
+    }
+
+    function newQuestionACB(){
+        props.onNewQuestionACB()
+    }
     return (
         <div className="answer-grid-container">
-          <div id='question-card' className={`${enableClass} animate__animated animate__fadeInDown`} onClick={newQuestionACB}>
+          <div id='question-card' className={`${props.enabledQuestion} animate__animated animate__fadeInDown`} onClick={newQuestionACB}>
             <div>{props.question}</div>
-            <div id="next-question" className={`${enableClass} animate__animated animate__fadeIn`}>Next question <FiArrowRight/></div>
+            <div id="next-question" className={`${props.enabledQuestion} animate__animated animate__fadeIn`}>Next question <FiArrowRight/></div>
           </div>
           <Answer model={props.model} 
             answer={props.answers[0]} 
-            currentAnswer={currentAnswer}
+            currentAnswer={props.currentAnswer}
             correctAnswer={props.correctAnswer}
             onUpdateCurrentAnswer={updateCurrentAnswerACB}/>
           <Answer model={props.model} 
             answer={props.answers[1]} 
+            currentAnswer={props.currentAnswer}
             correctAnswer={props.correctAnswer}
             onUpdateCurrentAnswer={updateCurrentAnswerACB}/>
           <Answer model={props.model} 
             answer={props.answers[2]} 
+            currentAnswer={props.currentAnswer}
             correctAnswer={props.correctAnswer}
             onUpdateCurrentAnswer={updateCurrentAnswerACB}/>
           <Answer model={props.model} 
             answer={props.answers[3]} 
+            currentAnswer={props.currentAnswer}
             correctAnswer={props.correctAnswer}
             onUpdateCurrentAnswer={updateCurrentAnswerACB}/>
         </div>
