50137ed10b168c8324074be4139ff0ea4fbbfba9
Edina Andreea Niculescu <eani@kth.se>
2022-12-13 13:59:47 +0100

    Leaderboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
+import GameResultsView from './Views/gameResultsView/gameResultsView';
+import Show from './components/show/show';
+import Game from './Presenters/gamePresenter';
+import GameModel from './GameModel';
 import Home from './Presenters/homePresenter';
-//import LogIn from './Presenters/loginPresenter';
-//import SidebarView from './Views/sidebarView/sidebarView';
+import LogIn from './Presenters/loginPresenter';
+import SidebarView from './Views/sidebarView/sidebarView';
+import Leaderboard from './Presenters/leaderboardPresenter';
 
 export default
-function App(props) {
+
+
+function App() {
+  const model = new GameModel();
   return (
     <div className="App">
-      <Home/>
-    </div>
+      <SidebarView/>
+      <Show hash="#gameResults">
+        <GameResultsView model={model}/>
+      </Show>
+      <Game model={model}/>
+      <Show hash="#home">
+        <Home/>
+      </Show>
+      <Show hash= "#login">
+        <LogIn/>
+      </Show>
+      <Show hash="#leaderboard">
+        <Leaderboard/>
+      </Show> 
+
+      </div>
   );
 }
 
