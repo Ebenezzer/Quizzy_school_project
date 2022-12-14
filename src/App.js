e77904c9f216a3fdf033a6886b10b38c59892322
Matilda Forslund <matforsl@kth.se>
2022-12-14 16:21:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import './App.css';
-import GameResultsView from './Views/gameResultsView/gameResultsView';
-import Show from './components/show/show';
-import Game from './Presenters/gamePresenter';
 import GameModel from './GameModel';
-import Home from './Presenters/homePresenter';
-import LogIn from './Presenters/loginPresenter';
-import SidebarView from './Views/sidebarView/sidebarView';
-import Leaderboard from './Presenters/leaderboardPresenter';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
+const GameResults=require('./Presenters/gameResultsPresenter.js').default;
+const Game=require('./Presenters/gamePresenter.js').default;
+const Home=require('./Presenters/homePresenter.js').default;
+const LogIn=require('./Presenters/loginPresenter.js').default;
+const Signup=require('./Presenters/signupPresenter.js').default;
+const Sidebar=require('./Views/sidebarView/sidebarView.js').default;
+
 
 export default
-
 
 function App() {
   const model = new GameModel();
   return (
-    <div className="App">
-      <SidebarView/>
-      <Show hash="#gameResults">
-        <GameResultsView model={model}/>
-      </Show>
-      <Game model={model}/>
-      <Show hash="#home">
-        <Home/>
-      </Show>
-      <Show hash= "#login">
-        <LogIn/>
-      </Show>
-      <Show hash="#leaderboard">
-        <Leaderboard/>
-      </Show> 
-
-      </div>
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element= {<Sidebar/>}> 
+          <Route path ="gameresults" element= {<GameResults model={model}/>}/>
+          <Route path ="game" element= {<Game model={model}/>}/>
+          <Route path ="home" element= {<Home model = {model}/>}/>
+          <Route path ="login" element= {<LogIn model = {model}/>}/> {/* what if I want path to be "/" so that's the first thing that is rendered when app is passed */}
+          <Route path ="signup" element= {<Signup model = {model}/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
-
